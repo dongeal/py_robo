e15dd1ef7f400cda624c12b90346d5b742ca19c9
dongeal <dongeal@gmail.com>
2022-06-17 10:17:01 +0900

    6/17 회사에서.... pywin32 설치 해서.. 엑셀 파일을 열었다 저장함으로 수식이 반영된 data_only 사용하여 그 값을 사용하게함.

--- 1_exel/17_quiz.py
+++ 1_exel/17_quiz.py
@@ -14,11 +14,64 @@ scores=[
 (7,	4,	9,	10,	16,	27,	18),
 (8,	6,	6,	6,	15,	19,	17),
 (9,	10,	10,	9,	19,	30,	19),
-(10,	9,	8,	8,	20,	25,	20)]
+(10, 9,	8,	8,	20,	25,	20)]
 
 for s in scores:
     ws.append(s)
 
+# 1.퀴즈2를 전부 10점으로
+for row in range(2, ws.max_row+1):
+    ws["D"+str(row)].value = 10
+
+# 2. H열에 총점,I 열에 성적 삽입
+ws["H1"]="총점"
+ws["I1"]="성적"
+for r in range(2, ws.max_row+1):
+    ws.cell(row=r,column=8).value = "=SUM(B{}:G{})".format(r,r)
+
+wb.save("scores.xlsx")   
+
+import win32com.client
+# pip install pywin32
+
+def LoadExcelFunction():
+    #win32com
+    #excel 사용할 수 있게 설정
+    excel = win32com.client.Dispatch("Excel.Application")
+    #임시 Workbook 객체 생성 및 엑셀 열기
+    temp_wb = excel.Workbooks.Open(
+        r"C:\Users\ilheu\Desktop\py_rpa\1_exel\scores.xlsx")
+    #저장
+    temp_wb.Save()
+    #excel 종료
+    excel.quit()
+    
+LoadExcelFunction()
+
+from openpyxl import load_workbook
+wb=load_workbook("scores.xlsx",data_only=True)
+ws=wb.active     
+
+for r in range(2, ws.max_row+1):
+    if ws.cell(row=r,column=2).value < 5 :
+        ws.cell(row=r,column=9).value ="F"
+        continue
+    if ws.cell(row=r,column=8).value >= 90:
+        ws.cell(row=r,column=9).value ="A"
+        continue
+    if ws.cell(row=r,column=8).value >= 80:
+        ws.cell(row=r,column=9).value ="B"
+        continue
+    if ws.cell(row=r,column=8).value >= 70:
+        ws.cell(row=r,column=9).value ="C"
+        continue
+    if ws.cell(row=r,column=8).value >= 60:
+        ws.cell(row=r,column=9).value ="D"
+        continue
+    else:
+        ws.cell(row=r,column=9).value ="D"
+
+wb.save("scores.xlsx")   
 
 
-wb.save("scores.xlsx")
+
